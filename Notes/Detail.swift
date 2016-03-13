cd2ce5c0d4b97cfab2f7692a2aa7a8c3a923793f
Kilian Koeltzsch <me@kilian.io>
2016-03-13 16:19:34 +0100

    Detail hinzugefügt

---
+++ Notes/Detail.swift
@@ -0,0 +1,19 @@
+//
+//  Detail.swift
+//  Notes
+//
+//  Created by Kilian Költzsch on 13/03/16.
+//  Copyright © 2016 SwiftDe. All rights reserved.
+//
+
+import UIKit
+
+class Detail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
